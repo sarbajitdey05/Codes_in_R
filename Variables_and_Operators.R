8d199b2c5e8fb8c976550b548a4f65975496c587
Sarbajit Dey <155373999+sarbajitdey05@users.noreply.github.com>
2024-01-02 11:13:43 +0530

    Add files via upload

---
+++ Variables_and_Operators.R
@@ -0,0 +1,37 @@
+# Variables in R
+
+# 2 ways to declare variables: '=' or '<-'
+
+a=10 # Double
+
+A<-20 # Double
+
+b="Hello" # Character
+
+c=3L # Integer
+
+d=3+6i # Complex
+
+e=TRUE # Logical
+
+typeof(e) # To know the type of a variable
+ls() # To know all the available variables
+rm() # To remove variable
+
+# Operators
+
+x=30
+y=10
+
+x+y # Addition
+x-y # Subtraction
+x*y # Multiplication
+x/y # Division
+x%%y # Modulus
+x^y # Exponent
+
+# Relational operators
+
+x>y # Greater than
+x<y # Less than
+x==y # Equal
